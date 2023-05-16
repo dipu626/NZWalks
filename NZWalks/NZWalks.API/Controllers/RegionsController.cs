c1fcc6bd35eb4775b13dca6a3517500d9d500598
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-05-16 23:06:58 +0600

    25. Get All Regions Action Method Implemented

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext _dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Region> regions = _dbContext.Regions.ToList();
+            
+            return Ok(regions);
+        }
+    }
+}
