e0b416c5644e543c0bce10917b7d9e17584ff288
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-05-16 23:22:32 +0600

    26. Get Region By Id Action Method

--- NZWalks/NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -20,8 +20,24 @@ namespace NZWalks.API.Controllers
         public IActionResult GetAll()
         {
             List<Region> regions = _dbContext.Regions.ToList();
-            
+
             return Ok(regions);
+        }
+
+        // GET SINGLE REGION (Get Region By ID)
+        // GET: https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{regionId:Guid}")]
+        public IActionResult GetById([FromRoute] Guid regionId)
+        {
+            Region? region = _dbContext.Regions.FirstOrDefault(x => x.Id == regionId);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(region);
         }
     }
 }
