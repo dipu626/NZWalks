f6299f6c42f9fcfdec0d454b89d77f488b54b3ed
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-07-03 23:12:02 +0600

    40. Seeding Data Using Entity Framework Core

--- NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -13,5 +13,83 @@ namespace NZWalks.API.Data
         public DbSet<Difficulty> Difficulties { get; set; }
         public DbSet<Region> Regions { get; set; }
         public DbSet<Walk> Walks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seed data for Difficulties
+            // Easy, Medium, Hard
+            List<Difficulty> difficulties = new()
+            {
+                new Difficulty
+                {
+                    Id = Guid.Parse("d43c3a76-b592-41a1-a913-beb0f2a6a87f"),
+                    Name = "Easy"
+                },
+                new Difficulty
+                {
+                    Id = Guid.Parse("6e582ba0-60bc-4bad-9934-d7dd1df5a50c"),
+                    Name = "Medium"
+                },
+                new Difficulty
+                {
+                    Id = Guid.Parse("ec1bea8a-bd82-4514-bcb8-685a73a32e3d"),
+                    Name = "Hard"
+                },
+            };
+
+            // Seed difficulties to the database 
+            modelBuilder.Entity<Difficulty>().HasData(difficulties);
+
+            // Seed data for Regions
+            List<Region> regions = new()
+            {
+                new Region
+                {
+                    Id = Guid.Parse("f7248fc3-2585-4efb-8d1d-1c555f4087f6"),
+                    Name = "Auckland",
+                    Code = "AKL",
+                    RegionImageUrl = "https://images.pexels.com/photos/5169056/pexels-photo-5169056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("6884f7d7-ad1f-4101-8df3-7a6fa7387d81"),
+                    Name = "Northland",
+                    Code = "NTL",
+                    RegionImageUrl = null
+                },
+                new Region
+                {
+                    Id = Guid.Parse("14ceba71-4b51-4777-9b17-46602cf66153"),
+                    Name = "Bay Of Plenty",
+                    Code = "BOP",
+                    RegionImageUrl = null
+                },
+                new Region
+                {
+                    Id = Guid.Parse("cfa06ed2-bf65-4b65-93ed-c9d286ddb0de"),
+                    Name = "Wellington",
+                    Code = "WGN",
+                    RegionImageUrl = "https://images.pexels.com/photos/4350631/pexels-photo-4350631.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("906cb139-415a-4bbb-a174-1a1faf9fb1f6"),
+                    Name = "Nelson",
+                    Code = "NSN",
+                    RegionImageUrl = "https://images.pexels.com/photos/13918194/pexels-photo-13918194.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("f077a22e-4248-4bf6-b564-c7cf4e250263"),
+                    Name = "Southland",
+                    Code = "STL",
+                    RegionImageUrl = null
+                },
+            };
+
+            modelBuilder.Entity<Region>().HasData(regions);
+        }
     }
 }
