9b72e09716af934d155c8608d351b774a2ed0db2
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-07-18 10:49:11 +0600

    60. Sedding Roles

--- NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ NZWalks/NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalks.API.Data
@@ -7,6 +8,34 @@ namespace NZWalks.API.Data
     {
         public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            string readerRoleId = "23daa7f8-e875-4b74-920a-a3d6a1151116";
+            string writerRoleId = "4e21912f-a64f-403e-b710-a319aa1a7a10";
+
+            List<IdentityRole> roles = new()
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName= "Writer".ToUpper(),
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
