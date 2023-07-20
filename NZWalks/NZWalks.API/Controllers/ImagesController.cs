ed198bde63d0b2a000f9684cefe97752d60b52a0
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-07-20 09:47:40 +0600

    72. New Controller and Action Method for Image Upload

---
+++ NZWalks/NZWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO.Images;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        // POST: /api/Images/Upload
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromBody] ImageUploadRequestDto imageUploadRequestDto)
+        {
+            ValidateFileUpload(imageUploadRequestDto);
+
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+
+
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDto imageUploadRequestDto)
+        {
+            string[] allowdExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+            if (allowdExtensions.Contains(Path.GetExtension(imageUploadRequestDto.FileName)) == false)
+            {
+                ModelState.AddModelError("file", "Unsupported file extension");
+            }
+
+            if (imageUploadRequestDto.File.Length > 3145728) // 3MB
+            {
+                ModelState.AddModelError("file", "File size more than 3 MB, please upload a smaller size file");
+            }
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Models/DTO/Images/ImagesUploadRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTO.Images
+{
+    public class ImageUploadRequestDto
+    {
+        [Required]
+        public required IFormFile File { get; set; }
+
+        [Required]
+        public required string FileName { get; set; }
+
+
+        public string? FileDescription { get; set; }
+    }
+}
