6bd4aea0dc1de86ced017da71a7644b545448bba
Dipu Mohanto <dipu.mohanto@selise.ch>
2023-07-22 00:22:45 +0600

    81. Implement Versioning By Nuget Package - Proffered Approach

---
+++ NZWalks/Web-API-Versioning.API/Controllers/CountriesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_API_Versioning.API.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    public class CountriesController : ControllerBase
+    {
+        [HttpGet]
+        [MapToApiVersion("1.0")]
+        public IActionResult GetV1()
+        {
+            List<Models.Domain.Country> countriesDomainModel = CountriesData.Get();
+
+            List<Models.DTO.CountryDtoV1> response = new();
+            foreach (Models.Domain.Country country in countriesDomainModel)
+            {
+                response.Add(new Models.DTO.CountryDtoV1
+                {
+                    Id = country.Id,
+                    Name = country.Name,
+                });
+            }
+            
+            return Ok(response);
+        }
+
+        [HttpGet]
+        [MapToApiVersion("2.0")]
+        public IActionResult GetV2()
+        {
+            List<Models.Domain.Country> countriesDomainModel = CountriesData.Get();
+
+            List<Models.DTO.CountryDtoV2> response = new();
+            foreach (Models.Domain.Country country in countriesDomainModel)
+            {
+                response.Add(new Models.DTO.CountryDtoV2
+                {
+                    Id = country.Id,
+                    CountryName = country.Name,
+                });
+            }
+
+            return Ok(response);
+        }
+    }
+}
---
+++ NZWalks/Web-API-Versioning.API/CountriesData.cs
@@ -0,0 +1,21 @@
+﻿using Web_API_Versioning.API.Models.Domain;
+
+namespace Web_API_Versioning.API
+{
+    public static class CountriesData
+    {
+        public static List<Country> Get()
+        {
+            List<Country> countries = new()
+            {
+                new Country{ Id = 1, Name = "Bangladesh" },
+                new Country{ Id = 2, Name = "India" },
+                new Country{ Id = 3, Name = "Nepal" },
+                new Country{ Id = 4, Name = "Pakistan" },
+                new Country{ Id = 5, Name = "Bhutan" },
+                new Country{ Id = 6, Name = "Thailand" },
+            };
+            return countries;
+        }
+    }
+}
